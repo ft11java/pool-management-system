7689bd0e7ec0fbe39b7621a4cc5cd1d396525936
ft11java <bilisim.tanersenturk@gmail.com>
2021-11-05 21:37:08 +0300

    first commit

--- src/main/java/com/havuzentity/havuzentitty/model/Pool.java
+++ src/main/java/com/havuzentity/havuzentitty/model/Pool.java
@@ -22,6 +22,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
